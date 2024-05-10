c0d0be27704e92f62d700d8a81264a0687444e5a
rajan-sc <rajanchauhan0608@gmail.com>
2024-05-10 14:51:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import question_model import Question
+from data import question_data
+
+question_bank = []
+
+def k():
+      for i in question_data
+        question_text = i["text"]
+        question_answer = i["answer"]
