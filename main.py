b99a6f24282a8f9f150d5e9ba9ea9022fbee1dbe
Rajan Singh <rajanchauhan0608@gmail.com>
2024-05-10 16:44:49 +0530

    While loop remaining

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import question_model import Question
+from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["answer"]
+    new_question = Question(text=question_text, answer=question_answer)
+    question_bank.append(new_question)
 
-def k():
-      for i in question_data
-        question_text = i["text"]
-        question_answer = i["answer"]
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
